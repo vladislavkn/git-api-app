62a4cea5faf6cc998de4ca8ea9fbbbc648a857a7
imnotoriginal <imnotoriginal@users.noreply.github.com>
2020-07-10 10:53:54 +0300

    add store

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,22 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  state: {
+    repos: [],
+  },
+  mutations: {
+    setRepos(state, payload) {
+      state.repos = payload;
+    },
+  },
+  actions: {
+    async fetchRepos({ commit }, userName) {
+      const repos = await fetch(
+        `https://api.github.com/users/${userName}/repos`
+      ).then((res) => res.json());
+
+      commit("setRepos", repos);
+    },
+  },
+  modules: {},
 });
